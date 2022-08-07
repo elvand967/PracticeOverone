2c71cf070fa75bfc82953e2289c358d145ce6bb0
elvand967 <110315572+elvand967@users.noreply.github.com>
2022-08-07 11:44:25 +0300

    practice No. 04/1 practical work completed

--- practice04-1.py
+++ practice04-1.py
@@ -5,3 +5,5 @@
 import random
 mytuple = [random.randint(0,100) for i in range(10)]
 print(mytuple)
+print(f'индекс min значения "{min(mytuple)}": [{mytuple.index(min(mytuple))}],\n'
+      f'индекс max значения "{max(mytuple)}": [{mytuple.index(max(mytuple))}]')
