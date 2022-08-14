77081022093777046ccfc23c6ae3828618e994e2
elvand967 <110315572+elvand967@users.noreply.github.com>
2022-08-14 06:45:37 +0300

    practice No. 06/1 practical work completed (overone)

---
+++ practice06-1(overone).py
@@ -0,0 +1,6 @@
+# Практическое задание №06-1  (Overone)
+# Создайте словарь person, в котором будут
+# присутствовать ключи name, age, city.
+# Выведите значение возраста из словаря person
+person ={'name':"Yana", 'age': 24, 'city':'Minsk'}
+print(person['age'])
